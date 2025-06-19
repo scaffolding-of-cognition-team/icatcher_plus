6166ccf85cce6688ad8c2b945cb36d718587c28d
CameronTEllis <cte@stanford.edu>
2025-06-19 14:19:57 -0700

    init

--- reproduce/convert_LookIt_coder_file_csv.m
+++ reproduce/convert_LookIt_coder_file_csv.m
@@ -19,7 +19,7 @@ function convert_LookIt_coder_file_csv(coder_file, output_csv)
     Codes = Output.Experiment;
 
     % Check if the coder finished
-    if (exist(FrameList) == 0) || (length(Frame_names) < length(FrameList))
+    if (exist('FrameList') == 0) || (length(Frame_names) < length(FrameList))
         fprintf('%s only finished %d of %d frames. Quitting without returning output\n', coder_file, length(Frame_names), length(FrameList))
         return
     end
